e9355eb19293bb85bdca53e61310c339727fa927
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-04-22 15:39:26 -0400

    blah

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
+
 var utp = require('utp');
-var host = process.argv[2] || '72.76.47.91';
-var port = Number(process.argv[3] || 25667);
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2), {
+  alias: {
+    h: 'host',
+    l: 'localport',
+    r: 'remoteport'
+  }
+})
+
+var DEFAULT_PORT = 25788
+var host = args.host || '72.76.47.91';
+var port = Number(args.localport || DEFAULT_PORT);
+var remotePort = Number(args.remoteport || DEFAULT_PORT);
 var closed = 0;
 var onclose = function() {
   if (++closed === 2) process.exit(0);
 };
 
 utp.createServer(function(socket) {
-  // socket.resume();
   console.log('new connection');
+  socket.write('hello from server');
   socket.on('end', function() {
     socket.end();
   });
   socket.on('close', onclose);
+  socket.on('data', function(e) {
+    console.log('Got data', e.toString());
+  });
 }).listen(port);
 
 console.log('attempting to connect to', host + ':' + port);
-var socket = utp.connect(port, host);
+var socket = utp.connect(remotePort, host);
 socket.on('connect', function() {
   console.log('connected');
 });
 
-// socket.resume();
 socket.on('close', onclose);
+socket.on('data', function(e) {
+  console.log('Got data', e.toString());
+});
+
+socket.write('hello from client');
+socket.end();
