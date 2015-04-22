ce66d7bb5b0537a5a1c54fdf65aaa1db822fb6fd
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-04-22 10:52:16 -0400

    log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var utp = require('utp');
-var host = process.argv[2];
+var host = process.argv[2] || '72.76.47.91';
+var port = Number(process.argv[3] || 25667);
 var closed = 0;
 var onclose = function() {
   if (++closed === 2) process.exit(0);
@@ -7,13 +8,18 @@ var onclose = function() {
 
 utp.createServer(function(socket) {
   // socket.resume();
+  console.log('new connection');
   socket.on('end', function() {
     socket.end();
   });
   socket.on('close', onclose);
-}).listen(12345);
+}).listen(port);
 
-var socket = utp.connect(12345, host || '72.76.47.91');
+console.log('attempting to connect to', host + ':' + port);
+var socket = utp.connect(port, host);
+socket.on('connect', function() {
+  console.log('connected');
+});
 
 // socket.resume();
 socket.on('close', onclose);
